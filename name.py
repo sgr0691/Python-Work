d104d3edc70d0745318db9191ebb0ac586a9b994
[sgr0691] <sgr0691@gmail.com>
2016-07-06 19:09:51 -0400

    Created Name file with two sections

---
+++ name.py
@@ -0,0 +1,33 @@
+# TRY IT YOURSELF
+# Write a separate program to accomplish each of these exercises. Save each program with a filename that follows standard Python conventions, using lowercase letters and underscores, such as simple_message.py and simple_messages.py.
+# 2-1. Simple Message: Store a message in a variable, and then print that message.
+# 2-2. Simple Messages: Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message.
+
+# Changing Case in a String with Methods
+name = "ada lovelace"
+print(name.title())
+
+# Second Activity
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+# Combining or Concatenating Strings
+# First Exercise
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+#Second Exercise
+first_name = "ada"
+last_name = "lovelace"
+# full_name = first_name + " " last_name
+print("Hello, " + full_name.title() + "!")
+
+#Third Exercise
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = first_name + " " + last_name
+message ="Hello, " + full_name.title() + "!"
+print(message)
