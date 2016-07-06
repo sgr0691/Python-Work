6e0ba2b71f4cb71641beb92e06bf16259b6f3196
[sgr0691] <sgr0691@gmail.com>
2016-07-06 19:18:38 -0400

    Created whitespace tutorial file

---
+++ whitespace.py
@@ -0,0 +1,9 @@
+# Adding Whitespace to Strings with Tabs or Newlines
+# Add a tab to your text, use the character combo \t
+print("\tPython")
+
+#Add a newsline in a string, use the character combo \n
+print("Languages:\nPython\nC\nJavascript")
+
+#The following example shows how you can use a one-line string to generate four lines of output:
+print("Languages:\n\tPython\n\tC\n\tJavascript")
